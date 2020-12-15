7418ada521320ff4109aa2ea85eec5376ca138f3
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-12-15 09:18:28 -0500

    refactor(util): use new __errno_location() on DragonflyBSD

--- src/util.rs
+++ src/util.rs
@@ -4,10 +4,10 @@ use std::io;
 use std::os::unix::prelude::*;
 use std::path::{Path, PathBuf};
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "dragonfly"))]
 pub use libc::__errno_location as errno_ptr;
 
-#[cfg(any(target_os = "freebsd", target_os = "dragonfly", target_os = "macos"))]
+#[cfg(any(target_os = "freebsd", target_os = "macos"))]
 pub use libc::__error as errno_ptr;
 
 #[cfg(any(target_os = "android", target_os = "netbsd", target_os = "openbsd"))]
