abac7d458d977d8015368dc7655e4f70ea3e7570
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-01-05 12:12:24 -0500

    refactor(constants): remove Solaris/Illumos definitions
    Currently, these OSes are not supported.

--- src/constants.rs
+++ src/constants.rs
@@ -9,20 +9,10 @@ pub const DIR_OPEN_FLAGS: libc::c_int = libc::O_PATH | libc::O_DIRECTORY;
 #[cfg(target_os = "freebsd")]
 pub const DIR_OPEN_FLAGS: libc::c_int = libc::O_EXEC | libc::O_DIRECTORY;
 
-// These OSes have an actual O_SEARCH flag
-#[cfg(any(target_os = "solaris", target_os = "illumos"))]
-pub const DIR_OPEN_FLAGS: libc::c_int = libc::O_SEARCH | libc::O_DIRECTORY;
-
 // No (known) equivalent of O_SEARCH on the current platform.
 // O_RDONLY will usually work OK, except when encountering directories with modes like --x------
 // (0o100). Unfortunately, we just won't be able to handle those cases on these platforms.
-#[cfg(not(any(
-    target_os = "linux",
-    target_os = "android",
-    target_os = "freebsd",
-    target_os = "solaris",
-    target_os = "illumos",
-)))]
+#[cfg(not(any(target_os = "linux", target_os = "android", target_os = "freebsd")))]
 pub const DIR_OPEN_FLAGS: libc::c_int = libc::O_RDONLY | libc::O_DIRECTORY;
 
 // Linux's default (it seems sysconf(_SC_SYMLOOP_MAX) always fails on glibc, and this is a
