7dabf55607c5406490db9fbb9f7bfd91c7c8360b
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-05 13:33:49 -0400

    refactor(mntid): use dev_t for MountId on non-Linux
    Instead of just casting to a u64.

--- src/mntid/unix.rs
+++ src/mntid/unix.rs
@@ -2,10 +2,10 @@ use std::io;
 use std::os::unix::prelude::*;
 
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
-pub struct MountId(u64);
+pub struct MountId(libc::dev_t);
 
 #[inline]
 pub fn identify_mount(fd: RawFd) -> io::Result<MountId> {
     let st = crate::util::fstat(fd)?;
-    Ok(MountId(st.st_dev as u64))
+    Ok(MountId(st.st_dev))
 }
